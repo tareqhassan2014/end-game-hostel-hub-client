70392f639d8f1c98d9a5ef05485108a62738ebc0
MdRashidul-Islam <rashidulislam.official1@gmail.com>
2022-04-05 12:02:48 +0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
+import Cart from './views/components/common/Cart';
 import TheLayout from './views/TheLayout';
 
 function App() {
     return (
         <BrowserRouter>
             <TheLayout />
+            <Cart />
         </BrowserRouter>
     );
 }
---
+++ src/views/components/common/Cart.tsx
@@ -0,0 +1,38 @@
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Typography,
+} from '@mui/material';
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                component="img"
+                height="140"
+                image="/static/images/cards/contemplative-reptile.jpg"
+                alt="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
+    );
+};
+
+export default Cart;
