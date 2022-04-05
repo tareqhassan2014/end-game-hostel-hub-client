c2e1f3c465c68bb4ba6c011626207035a59aa347
Tareq Hassan <tareqhassan2014@gmail.com>
2022-04-05 17:58:09 +0600

    add huski

--- src/views/components/banner/banner.tsx
+++ src/views/components/banner/banner.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const banner = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    return <div></div>;
 };
 
 export default banner;
--- src/views/pages/contact/Contact.tsx
+++ src/views/pages/contact/Contact.tsx
@@ -2,21 +2,21 @@ import { Typography } from '@mui/material';
 import { Box } from '@mui/system';
 
 function Contact() {
-  return (
-      <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        height: '90vh',
-        width: '95vw',
-        justifyContent: 'center',
-      }}
-    >
-          <Typography variant="h2" align="center">
-              Contact page
-          </Typography>
-      </Box>
-  );
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                height: '90vh',
+                width: '95vw',
+                justifyContent: 'center',
+            }}
+        >
+            <Typography variant="h2" align="center">
+                Contact page
+            </Typography>
+        </Box>
+    );
 }
 
 export default Contact;
