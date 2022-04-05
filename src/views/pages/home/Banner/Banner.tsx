02c9cf17c573639dd19544d67f8ca1219b18bbad
MdHasanRaju <mdhasanraju2017@gmail.com>
2022-04-05 12:04:18 +0600

    raju

---
+++ src/views/pages/home/Banner/Banner.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Banner = () => {
+    return <div></div>;
+};
+
+export default Banner;
---
+++ src/views/pages/home/DisplayBanner/DisplayBanner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DisplayBanner = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DisplayBanner;
