4bd1cf406e277898e42096be35a4f6789e83defc
amirul-islam <amirulislam.shanto.75@gmail.com>
2022-04-06 23:59:53 +0600

    Update Regestrasion

--- src/views/components/signUp/AdminSignup.tsx
+++ src/views/components/signUp/AdminSignup.tsx
@@ -1,23 +1,258 @@
-import { Typography } from '@mui/material';
-import { Box } from '@mui/system';
-import React from 'react';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import LoadingButton from '@mui/lab/LoadingButton';
+import {
+    Avatar,
+    Box,
+    Checkbox,
+    Container,
+    CssBaseline,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    Grid,
+    Link,
+    Radio,
+    RadioGroup,
+    TextField,
+    Typography,
+} from '@mui/material';
+import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Link as DomLink } from 'react-router-dom';
 
-const AdminSignup = () => {
-    return (
-        <Box
-            sx={{
-                display: 'flex',
-                alignItems: 'center',
-                height: '90vh',
-                width: '95vw',
-                justifyContent: 'center',
-            }}
-        >
-            <Typography variant="h3" align="center">
-                This is admin Signup page
-            </Typography>
-        </Box>
-    );
+type Inputs = {
+    email: string;
+    location: string;
+    password: string;
+    phone: string;
+    name: string;
+    ConfirmPassword: string;
+    hostelType: string;
 };
 
-export default AdminSignup;
+export default function AdminSignup() {
+    const [show, setShow] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        watch,
+    } = useForm<Inputs>();
+    const password = watch('password');
+
+    const onSubmit: SubmitHandler<Inputs> = (forData) => {
+        console.log(forData);
+    };
+
+    return (
+        <Container component="main" maxWidth="lg">
+            <CssBaseline />
+
+            <Grid container spacing={2}>
+                <Grid item display={{ xs: 'none', md: 'block' }} md={7} lg={8}>
+                    <Box
+                        component="img"
+                        style={{ width: '100%' }}
+                        sx={{
+                            marginTop: 20,
+                        }}
+                        alt="The house from the offer."
+                        src="https://i.ibb.co/WsrDN8m/20944167.jpg"
+                    />
+                </Grid>
+                <Grid item xs={12} md={5} lg={4}>
+                    <Box
+                        sx={{
+                            marginTop: 10,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <HomeOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Creatre Hostel/Mess
+                        </Typography>
+                        <Box
+                            component="form"
+                            onSubmit={handleSubmit(onSubmit)}
+                            noValidate
+                            sx={{ mt: 1 }}
+                        >
+                            <TextField
+                                required
+                                autoFocus
+                                fullWidth
+                                autoComplete="name"
+                                margin="normal"
+                                error={Boolean(errors.name)}
+                                label={errors.name ? 'Error' : 'Hostel Name'}
+                                helperText={errors.name?.message}
+                                {...register('name', {
+                                    required: 'Hostel name is required',
+                                })}
+                            />
+                            <TextField
+                                required
+                                autoFocus
+                                fullWidth
+                                autoComplete="location"
+                                margin="normal"
+                                error={Boolean(errors.name)}
+                                label={
+                                    errors.location
+                                        ? 'Error'
+                                        : 'Hostel location'
+                                }
+                                helperText={errors.location?.message}
+                                {...register('location', {
+                                    required: 'Hostel location is required',
+                                })}
+                            />
+                            <TextField
+                                fullWidth
+                                required
+                                margin="normal"
+                                error={Boolean(errors.phone)}
+                                label={errors.phone ? 'Error' : 'Phone Number'}
+                                helperText={errors.phone?.message}
+                                autoComplete="phone"
+                                {...register('phone', {
+                                    required: 'phone is required',
+                                })}
+                            />
+                            <TextField
+                                fullWidth
+                                required
+                                margin="normal"
+                                error={Boolean(errors.email)}
+                                label={errors.email ? 'Error' : 'Email Address'}
+                                helperText={errors.email?.message}
+                                autoComplete="email"
+                                {...register('email', {
+                                    required: 'Email is required',
+                                })}
+                            />
+
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    '& > :nth-child(1)': { mr: 2 },
+                                }}
+                            >
+                                <TextField
+                                    fullWidth
+                                    required
+                                    margin="normal"
+                                    type={show ? 'text' : 'password'}
+                                    error={Boolean(errors.password)}
+                                    label={
+                                        errors.password ? 'Error' : 'Password'
+                                    }
+                                    helperText={errors.password?.message}
+                                    autoComplete="current-password"
+                                    {...register('password', {
+                                        required: 'password is required',
+                                    })}
+                                />
+
+                                <TextField
+                                    fullWidth
+                                    required
+                                    margin="normal"
+                                    type={show ? 'text' : 'password'}
+                                    error={Boolean(errors.ConfirmPassword)}
+                                    label={
+                                        errors.ConfirmPassword
+                                            ? 'Error'
+                                            : 'Confirm Password'
+                                    }
+                                    helperText={errors.ConfirmPassword?.message}
+                                    autoComplete="current-password"
+                                    {...register('ConfirmPassword', {
+                                        required: 'Confirm your password!',
+                                        validate: (value) =>
+                                            value === password ||
+                                            "password don't match",
+                                    })}
+                                />
+                            </Box>
+
+                            <Grid container spacing={2}>
+                                <Grid item xs={6} order={{ xs: 1, sm: 2 }}>
+                                    <FormControlLabel
+                                        sx={{
+                                            display: 'flex',
+                                            flexDirection: 'row-reverse',
+                                        }}
+                                        control={
+                                            <Checkbox
+                                                value="remember"
+                                                color="primary"
+                                            />
+                                        }
+                                        label="Show password"
+                                        onClick={() => setShow(!show)}
+                                    />
+                                </Grid>
+                                <Grid item xs={6} order={{ xs: 2, sm: 1 }}>
+                                    <FormControl>
+                                        <FormLabel id="demo-row-radio-buttons-group-label">
+                                            Hostel/Mess Type
+                                        </FormLabel>
+                                        <RadioGroup
+                                            row
+                                            aria-labelledby="demo-row-radio-buttons-group-label"
+                                        >
+                                            <FormControlLabel
+                                                value="boys"
+                                                control={<Radio />}
+                                                label="Boys"
+                                                {...register('hostelType', {
+                                                    required: true,
+                                                })}
+                                            />
+                                            <FormControlLabel
+                                                value="girls"
+                                                control={<Radio />}
+                                                label="Girls"
+                                                {...register('hostelType', {
+                                                    required: true,
+                                                })}
+                                            />
+                                        </RadioGroup>
+                                    </FormControl>
+                                </Grid>
+                            </Grid>
+
+                            <LoadingButton
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign In
+                            </LoadingButton>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Forgot password?
+                                    </Link>
+                                </Grid>
+                                <Grid item>
+                                    <DomLink to="/login">
+                                        Already Have a Hostel? login
+                                    </DomLink>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Container>
+    );
+}
--- src/views/pages/signIn/SignIn.tsx
+++ src/views/pages/signIn/SignIn.tsx
@@ -54,86 +54,101 @@ export default function SignIn() {
       toast.error(error?.data?.message);
     }
   };
+ return (
+     <Container component="main" maxWidth="lg">
+         <CssBaseline />
 
-  return (
-      <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                  <LockOutlinedIcon />
-              </Avatar>
-              <Typography component="h1" variant="h5">
-                  Sign In
-              </Typography>
-              <Box
-          component="form"
-          onSubmit={handleSubmit(onSubmit)}
-          noValidate
-          sx={{ mt: 1 }}
-        >
-                  <TextField
-            required
-            fullWidth
-            autoFocus
-            margin="normal"
-            autoComplete="email"
-            error={Boolean(errors.email)}
-            helperText={errors.email?.message}
-            label={
-                            errors.email ? 'Error' : 'Email Address'
-                        }
-            {...register('email', {
-              required: 'Email is required',
-            })}
-          />
-                  <TextField
-            required
-            fullWidth
-            margin="normal"
-            autoComplete="current-password"
-            error={Boolean(errors.password)}
-            type={show ? 'text' : 'password'}
-            label={errors.password ? 'Error' : 'Password'}
-            helperText={errors.password?.message}
-            {...register('password', {
-              required: 'password is required',
-            })}
-          />
-                  <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Show password"
-            onClick={() => setShow(!show)}
-          />
-                  <LoadingButton
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            loading={isLoading}
-          >
-                      Sign In
-                  </LoadingButton>
-                  <Grid container>
-                      <Grid item xs>
-                          <Link href="#" variant="body2">
-                              Forgot password?
-                          </Link>
-                      </Grid>
-                      <Grid item>
-                          <DomLink to="/signup">
-                              New here? Sign UP
-                          </DomLink>
-                      </Grid>
-                  </Grid>
-              </Box>
-          </Box>
-      </Container>
-  );
+         <Grid container spacing={2}>
+             <Grid item display={{ xs: 'none', lg: 'block' }} md={7} lg={8}>
+                 <Box
+                     component="img"
+                     style={{ width: '90%' }}
+                     sx={{
+                         marginTop: 8,
+           }}
+                     alt="The house from the offer."
+                     src="https:i.ibb.co/c1r2jDs/6333204.jpg"
+                 />
+             </Grid>
+             <Grid item xs={12}  lg={4}>
+                 <Box
+                     sx={{
+                         marginTop: 20,
+                         display: 'flex',
+                         flexDirection: 'column',
+                         alignItems: 'center',
+                     }}
+                 >
+                     <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                         <LockOutlinedIcon />
+                     </Avatar>
+                     <Typography component="h1" variant="h5">
+                         Sign In
+                     </Typography>
+                     <Box
+                         component="form"
+                         onSubmit={handleSubmit(onSubmit)}
+                         noValidate
+                         sx={{ mt: 1 }}
+                     >
+                         <TextField
+                             required
+                             fullWidth
+                             autoFocus
+                             margin="normal"
+                             autoComplete="email"
+                             error={Boolean(errors.email)}
+                             helperText={errors.email?.message}
+                             label={errors.email ? 'Error' : 'Email Address'}
+                             {...register('email', {
+                                 required: 'Email is required',
+                             })}
+                         />
+                         <TextField
+                             required
+                             fullWidth
+                             margin="normal"
+                             autoComplete="current-password"
+                             error={Boolean(errors.password)}
+                             type={show ? 'text' : 'password'}
+                             label={errors.password ? 'Error' : 'Password'}
+                             helperText={errors.password?.message}
+                             {...register('password', {
+                                 required: 'password is required',
+                             })}
+                         />
+                         <FormControlLabel
+                             control={
+                                 <Checkbox value="remember" color="primary" />
+                             }
+                             label="Show password"
+                             onClick={() => setShow(!show)}
+                         />
+                         <LoadingButton
+                             type="submit"
+                             fullWidth
+                             variant="contained"
+                             sx={{ mt: 3, mb: 2 }}
+                             loading={isLoading}
+                         >
+                             Sign In
+                         </LoadingButton>
+                         <Grid container>
+                             <Grid item xs>
+                                 <Link href="#" variant="body2">
+                                     Forgot password?
+                                 </Link>
+                             </Grid>
+                             <Grid item>
+                                 <DomLink to="/signup">
+                                     New here? Sign UP
+                                 </DomLink>
+                             </Grid>
+                         </Grid>
+                     </Box>
+                 </Box>
+             </Grid>
+         </Grid>
+     </Container>
+ );
 }
