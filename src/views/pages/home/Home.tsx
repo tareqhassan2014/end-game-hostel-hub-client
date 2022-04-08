cc4d8c46ee67eeca3ccff81b5e3f9f99dfd4a051
amirul-islam <amirulislam.shanto.75@gmail.com>
2022-04-08 10:41:15 +0600

    done service and facilitis

--- src/views/pages/home/Home.tsx
+++ src/views/pages/home/Home.tsx
@@ -2,7 +2,6 @@ import { Box } from '@mui/system';
 import Banner from 'src/views/components/banner/banner';
 import AllService from 'src/views/components/home/AllService';
 import FeatureApp from 'src/views/components/home/FeatureApp';
-import Raju from 'src/views/components/home/Raju';
 import Sponsors from 'src/views/components/home/Sponsors';
 
 function Home() {
