6ca56c62caa58818771b34b8ef0efcfc937f8d28
salman2090 <salman20909@gmail.com>
2022-04-12 14:55:29 +0600

    component rendered

--- src/views/pages/home/Home.tsx
+++ src/views/pages/home/Home.tsx
@@ -7,8 +7,8 @@ function Home() {
     return (
         <>
             <Banner />
-            <FeatureApp />
             <AllService />
+            <FeatureApp />
             <Sponsors />
         </>
     );
