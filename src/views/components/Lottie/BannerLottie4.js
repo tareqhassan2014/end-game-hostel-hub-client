8cd35848444c16be203ed754304e1f8ab7645655
MdHasanRaju <mdhasanraju2017@gmail.com>
2022-04-13 11:56:57 +0600

    lottie added newly

--- src/views/components/Lottie/BannerLottie4.js
+++ src/views/components/Lottie/BannerLottie4.js
@@ -1,9 +1,12 @@
 const BannerLottie2 = () => {
     return (
         // https://assets8.lottiefiles.com/packages/lf20_qdiq7qa5.json
+        //https://assets3.lottiefiles.com/packages/lf20_jtzgxdui.json
+        //https://assets3.lottiefiles.com/private_files/lf30_ti7aknf9.json
+        //https://assets7.lottiefiles.com/private_files/lf30_4ajtmk.json
         <lottie-player
-            src="https://assets8.lottiefiles.com/packages/lf20_qdiq7qa5.json"
-            style={{ width: 'inherit', height: 'inherit' }}
+            src="https://assets3.lottiefiles.com/private_files/lf30_ti7aknf9.json"
+            style={{ width: '100%', height: '100%' }}
             background="transparent"
             speed="1"
             loop
