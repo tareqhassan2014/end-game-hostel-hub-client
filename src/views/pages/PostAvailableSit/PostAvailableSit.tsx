ed924fcf7bf17f0aeaff92441315863623e6c93b
MdRashidul-Islam <rashidulislam.official1@gmail.com>
2022-04-17 23:01:53 +0600

    update

--- src/views/pages/PostAvailableSit/PostAvailableSit.tsx
+++ src/views/pages/PostAvailableSit/PostAvailableSit.tsx
@@ -12,27 +12,35 @@ const PostAvailableSit = () => {
                     <h3>Post Vacent Seat</h3>
                     <form onSubmit={handleSubmit(onSubmit)}>
                         <input {...register('firstName')} />
-                        <select {...register('roomSize')}>
+                        <select {...register('roomSize')} required>
                             <option value="">Room Size</option>
                             <option value="9 feet x 10 feet">
                                 9 feet x 10 feet
                             </option>
                             <option value="10 x 10 feet">10 x 10 feet</option>
                         </select>
-                        <select {...register('bedType')}>
+                        <select {...register('bedType')} required>
                             <option value="">Bed Type</option>
                             <option value="single-bed">Single Bed</option>
                             <option value="double-bed">Double Bed</option>
                         </select>
-                        <select {...register('city')}>
+                        <select {...register('city')} required>
                             <option value="">Location</option>
                             <option value="dhaka">Dhaka</option>
                         </select>
-                        <select {...register('gender')}>
+                        <select {...register('gender')} required>
+                            <option value="">Gender</option>
                             <option value="female">female</option>
                             <option value="male">male</option>
                         </select>
-                        <input type="submit" />
+                        <input
+                            type="number"
+                            {...register('phone')}
+                            placeholder="Phone"
+                            required
+                        />
+                        <input type="file" {...register('img')} />
+                        <input value="Post" type="submit" />
                     </form>
                 </div>
             </div>
