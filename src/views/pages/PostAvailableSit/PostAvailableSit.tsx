560c8938821998bf204c504d320bf51d1264059a
MdRashidul-Islam <rashidulislam.official1@gmail.com>
2022-04-17 22:05:19 +0600

    update

--- src/views/pages/PostAvailableSit/PostAvailableSit.tsx
+++ src/views/pages/PostAvailableSit/PostAvailableSit.tsx
@@ -6,34 +6,37 @@ const PostAvailableSit = () => {
     const { register, handleSubmit } = useForm();
     const onSubmit = (data: any) => console.log(data);
     return (
-        <div className="availableSit">
-            <div className="form">
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <input {...register('firstName')} />
-                    <select {...register('roomSize')}>
-                        <option value="">Room Size</option>
-                        <option value="9 feet x 10 feet">
-                            9 feet x 10 feet
-                        </option>
-                        <option value="10 x 10 feet">10 x 10 feet</option>
-                    </select>
-                    <select {...register('bedType')}>
-                        <option value="">Bed Type</option>
-                        <option value="single-bed">Single Bed</option>
-                        <option value="double-bed">Double Bed</option>
-                    </select>
-                    <select {...register('city')}>
-                        <option value="">Location</option>
-                        <option value="dhaka">Dhaka</option>
-                    </select>
-                    <select {...register('gender')}>
-                        <option value="female">female</option>
-                        <option value="male">male</option>
-                    </select>
-                    <input type="submit" />
-                </form>
+        <>
+            <div className="availableSit">
+                <div className="form">
+                    <h3>Post Vacent Seat</h3>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <input {...register('firstName')} />
+                        <select {...register('roomSize')}>
+                            <option value="">Room Size</option>
+                            <option value="9 feet x 10 feet">
+                                9 feet x 10 feet
+                            </option>
+                            <option value="10 x 10 feet">10 x 10 feet</option>
+                        </select>
+                        <select {...register('bedType')}>
+                            <option value="">Bed Type</option>
+                            <option value="single-bed">Single Bed</option>
+                            <option value="double-bed">Double Bed</option>
+                        </select>
+                        <select {...register('city')}>
+                            <option value="">Location</option>
+                            <option value="dhaka">Dhaka</option>
+                        </select>
+                        <select {...register('gender')}>
+                            <option value="female">female</option>
+                            <option value="male">male</option>
+                        </select>
+                        <input type="submit" />
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
