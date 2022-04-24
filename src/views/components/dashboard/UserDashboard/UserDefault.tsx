ae8157078ee999f79bbd2d84b73dc6afd56272c9
MdHasanRaju <mdhasanraju2017@gmail.com>
2022-04-24 10:36:10 +0600

    user line chart added

--- src/views/components/dashboard/UserDashboard/UserDefault.tsx
+++ src/views/components/dashboard/UserDashboard/UserDefault.tsx
@@ -1,41 +1,52 @@
 import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
-import { Box } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import CommonCart from '../common/CommonCart';
+import LineChart from './LineChart';
 
 const UserDefault = () => {
     return (
-        <div>
+        <Container>
             <Box
-                sx={{
+                style={{
                     display: 'flex',
-                    flexWrap: 'wrap',
-                    ml: 5,
-                    mt: 2,
-                    '& > :not(style)': {
-                        m: 1,
-                        width: 'auto',
-                        height: 'auto',
-                    },
+                    alignItems: 'center',
+                    justifyContent: 'center',
                 }}
             >
-                <CommonCart
-                    icon={<SearchIcon />}
-                    title="Title goes here"
-                    text="Description goes to here"
-                />
-                <CommonCart
-                    icon={<SearchIcon />}
-                    title="Title goes here"
-                    text="Description goes to here"
-                />
-                <CommonCart
-                    icon={<SearchIcon />}
-                    title="Title goes here"
-                    text="Description goes to here"
-                />
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        ml: 2,
+                        mt: 2,
+                        '& > :not(style)': {
+                            m: 1,
+                            width: 'auto',
+                            height: 'auto',
+                        },
+                    }}
+                >
+                    <CommonCart
+                        icon={<SearchIcon />}
+                        title="Title goes here"
+                        text="Description goes to here"
+                    />
+
+                    <CommonCart
+                        icon={<SearchIcon />}
+                        title="Title goes here"
+                        text="Description goes to here"
+                    />
+                    <CommonCart
+                        icon={<SearchIcon />}
+                        title="Title goes here"
+                        text="Description goes to here"
+                    />
+                </Box>
             </Box>
-        </div>
+            <LineChart />
+        </Container>
     );
 };
 
