4bfd4c356c143d87dbde38a906296ecbae21b8d6
MdHasanRaju <mdhasanraju2017@gmail.com>
2022-05-14 11:38:06 +0600

    counter added

---
+++ src/views/components/home/Counter.jsx
@@ -0,0 +1,196 @@
+import { Box, Grid, Typography, Container, Paper } from '@mui/material';
+import React, { useState } from 'react';
+import CountUp from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
+
+const Counter = () => {
+    const [focus, setFocus] = useState(false);
+
+    function onChange(isVisible) {
+        console.log('Element is now %s', isVisible ? 'visible' : 'hidden');
+    }
+
+    return (
+        <Box sx={{ my: 5 }}>
+            <Typography
+                variant="h3"
+                sx={{ textAlign: 'center', mb: 3, color: '#6A52E5' }}
+            >
+                Survey
+            </Typography>
+            <Container>
+                {/* only for first time display after loading the page-start */}
+                <Grid
+                    container
+                    rowSpacing={1}
+                    columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+                    sx={{ textAlign: 'center' }}
+                >
+                    <Grid item lg={3} md={3} sm={6} xs={12}>
+                        <Paper
+                            sx={{
+                                boxShadow: 'inset 0 2px 48px 0 rgb(0 0 0 / 4%)',
+                                borderRadius: '10px',
+                                padding: '30px',
+                                backgroundColor: '#f2ffff',
+                                transition: '.3s ease-in-out',
+                            }}
+                        >
+                            <Typography
+                                variant="h3"
+                                sx={{ fontFamily: "'Monoton',cursive" }}
+                            >
+                                <CountUp
+                                    start={focus ? 0 : null}
+                                    end={60}
+                                    duration={2}
+                                    redraw={true}
+                                    suffix=" +"
+                                >
+                                    {({ countUpRef }) => (
+                                        <VisibilitySensor
+                                            onChange={(isVisible) => {
+                                                if (isVisible) {
+                                                    setFocus(true);
+                                                }
+                                            }}
+                                        >
+                                            <span ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>
+                            </Typography>
+                            <Typography variant="subtitle1">
+                                {' '}
+                                Products Added
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                    <Grid item lg={3} md={3} sm={6} xs={12}>
+                        <Paper
+                            sx={{
+                                boxShadow: 'inset 0 2px 48px 0 rgb(0 0 0 / 4%)',
+                                borderRadius: '10px',
+                                padding: '30px',
+                                backgroundColor: '#f2ffff',
+                                transition: '.3s ease-in-out',
+                            }}
+                        >
+                            <Typography
+                                variant="h3"
+                                sx={{ fontFamily: "'Monoton',cursive" }}
+                            >
+                                <CountUp
+                                    start={focus ? 0 : null}
+                                    end={150}
+                                    duration={4}
+                                    redraw={true}
+                                    suffix=" +"
+                                >
+                                    {({ countUpRef }) => (
+                                        <VisibilitySensor
+                                            onChange={(isVisible) => {
+                                                if (isVisible) {
+                                                    setFocus(true);
+                                                }
+                                            }}
+                                        >
+                                            <span ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>
+                            </Typography>
+                            <Typography variant="subtitle1">
+                                {' '}
+                                Hostel Booked
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                    <Grid item lg={3} md={3} sm={6} xs={12}>
+                        <Paper
+                            sx={{
+                                boxShadow: 'inset 0 2px 48px 0 rgb(0 0 0 / 4%)',
+                                borderRadius: '10px',
+                                padding: '30px',
+                                backgroundColor: '#f2ffff',
+                                transition: '.3s ease-in-out',
+                            }}
+                        >
+                            <Typography
+                                variant="h3"
+                                sx={{ fontFamily: "'Monoton',cursive" }}
+                            >
+                                <CountUp
+                                    start={focus ? 0 : null}
+                                    end={45}
+                                    duration={1}
+                                    redraw={true}
+                                    suffix=" +"
+                                >
+                                    {({ countUpRef }) => (
+                                        <VisibilitySensor
+                                            onChange={(isVisible) => {
+                                                if (isVisible) {
+                                                    setFocus(true);
+                                                }
+                                            }}
+                                        >
+                                            <span ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>
+                            </Typography>
+                            <Typography variant="subtitle1">
+                                Store Created
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                    <Grid item lg={3} md={3} sm={6} xs={12}>
+                        <Paper
+                            sx={{
+                                boxShadow: 'inset 0 2px 48px 0 rgb(0 0 0 / 4%)',
+                                borderRadius: '10px',
+                                padding: '30px',
+                                backgroundColor: '#f2ffff',
+                                transition: '.3s ease-in-out',
+                            }}
+                        >
+                            <Typography
+                                variant="h3"
+                                sx={{ fontFamily: "'Monoton',cursive" }}
+                            >
+                                <CountUp
+                                    start={focus ? 0 : null}
+                                    end={130}
+                                    duration={3}
+                                    redraw={true}
+                                    suffix=" +"
+                                >
+                                    {({ countUpRef }) => (
+                                        <VisibilitySensor
+                                            onChange={(isVisible) => {
+                                                if (isVisible) {
+                                                    setFocus(true);
+                                                }
+                                            }}
+                                        >
+                                            <span ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>
+                            </Typography>
+                            <Typography variant="subtitle1">
+                                {' '}
+                                Products Sold
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                </Grid>
+
+                {/* only for first time display after loading the page-end */}
+            </Container>
+        </Box>
+    );
+};
+
+export default Counter;
--- src/views/pages/home/Home.tsx
+++ src/views/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import { CssBaseline } from '@mui/material';
 import AllService from 'src/views/components/home/AllService';
 import Banner from 'src/views/components/home/Banner';
+import Counter from 'src/views/components/home/Counter';
 import FeatureApp from 'src/views/components/home/FeatureApp';
 import Sponsors from 'src/views/components/home/Sponsors';
 
@@ -9,6 +10,7 @@ function Home() {
         <>
             <CssBaseline />
             <Banner />
+            <Counter />
             <AllService />
             <FeatureApp />
             <Sponsors />
